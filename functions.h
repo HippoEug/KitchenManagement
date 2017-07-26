2bf29ef950027eb99e1a8d7040dfa545113b9513
HippoEug <efoo10@gmail.com>
2017-07-26 18:51:47 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -3,14 +3,21 @@
 #include <string>
 using namespace std;
 
-void addNewItem(string categoryX, string productX, int productCodeX, float productPriceX)
+void addNewItem(int& categoryX, string& productX, int& productCodeX, float& productPriceX)
 {
-	cout << "Enter Product Category: ";
-	cin >> categoryX;
-	cout << "Enter Product Name: ";
-	cin >> productX;
-	cout << "Enter Product Code: ";
-	cin >> productCodeX;
-	cout << "Enter Product Price: $";
-	cin >> productPriceX;
+    cout << "\nEnter Category of Food Type" << endl;
+    cout << "Press [1]: Raw" << endl;
+    cout << "Press [2]: Fresh" << endl;
+    cout << "Press [3]: Precooked" << endl;
+    cout << "\nEnter Product Category: ";
+    cin >> categoryX;
+    
+    cout << "Enter Product Name: ";
+    cin >> productX;
+    
+    cout << "Enter Product Code: ";
+    cin >> productCodeX;
+    
+    cout << "Enter Product Price: $";
+    cin >> productPriceX;
 }
