61e84774a03aacc5d91113abcdef14d0fece7c14
Eugene <efoo10@gmail.com>
2017-07-30 16:54:27 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,21 +1,23 @@
 #pragma once
+#include <iostream>
+#include <string>
 using namespace std;
 
-void addNewItem(int& categoryX, string& productX, int& productCodeX, float& productPriceX)
+void addNewItem(int& categoryX, string& productX, int& productCodeX, long long& dateX)
 {
-    cout << "\nEnter Category of Food Type" << endl;
-    cout << "Press [1]: Raw" << endl;
-    cout << "Press [2]: Fresh" << endl;
-    cout << "Press [3]: Precooked" << endl;
-    cout << "\nEnter Product Category: ";
-    cin >> categoryX;
-    
-    cout << "Enter Product Name: ";
-    cin >> productX;
-    
-    cout << "Enter Product Code: ";
-    cin >> productCodeX;
-    
-    cout << "Enter Product Price: $";
-    cin >> productPriceX;
+	cout << "\nEnter Category of Food Type" << endl;
+	cout << "Press [1]: Raw" << endl;
+	cout << "Press [2]: Fresh" << endl;
+	cout << "Press [3]: Precooked" << endl;
+	cout << "\nEnter Product Category: ";
+	cin >> categoryX;
+
+	cout << "Enter Product Name: ";
+	cin >> productX;
+
+	cout << "Enter Product Code: ";
+	cin >> productCodeX;
+
+	cout << "Enter Product Expiry Date [DDMMYYYY]: ";
+	cin >> dateX;
 }
