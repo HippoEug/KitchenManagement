8b80e34e704216cc4dcb8967971c3c53c1d8e995
Eugene <efoo10@gmail.com>
2017-07-30 16:54:50 +0800

    Update items.h

--- items.h
+++ items.h
@@ -5,20 +5,18 @@ using namespace std;
 class item
 {
 public: //original private:
-    int category; //Categories: Raw, Fresh, Dairy
-    string product; //Products: Chocolate Milk, Butter
-    int productCode; //Product Code: 03421 (to modify this since programme is using FIFO structure
-    float productPrice; //Product Price: $30.20, also check if this is neccessary with client
-    //public:
-    item(); //constructor
-    //~item(); //deconstructor, delete if not necessary
-    //to make category, product, productCode and productPrice as private, hence write functions for it
+	int category; //Categories: Raw, Fresh, Dairy
+	string product; //Products: Chocolate Milk, Butter
+	int productCode; //Product Code: 03421 (to modify this since programme is using FIFO structure
+	long long productDate; //Product Expiry Date
+
+	item();
 };
 
 item::item() //constructor here, check defaulting with queue system
 {
-    category = 0; //change to choice, 1 = raw, 2 = fresh, 3 = precooked
-    product = "NULL";
-    productCode = 0;
-    productPrice = 0;
+	category = 0; //change to choice, 1 = raw, 2 = fresh, 3 = precooked
+	product = "NULL";
+	productCode = 0;
+	productDate = 0;
 }
