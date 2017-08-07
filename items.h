96a645136184e46d5cf220e3f6ef77353d20c9ce
Eugene <efoo10@gmail.com>
2017-08-08 01:15:10 +0800

    Update items.h

--- items.h
+++ items.h
@@ -8,7 +8,9 @@ public: //original private:
 	int category; //Categories: Raw, Fresh, Dairy
 	string product; //Products: Chocolate Milk, Butter
 	int productCode; //Product Code: 03421 (to modify this since programme is using FIFO structure
-	long long productDate; //Product Expiry Date
+	int dateDay;
+	int dateMonth;
+	int dateYear;
 
 	item();
 };
@@ -18,5 +20,7 @@ item::item() //constructor here, check defaulting with queue system
 	category = 0; //change to choice, 1 = raw, 2 = fresh, 3 = precooked
 	product = "NULL";
 	productCode = 0;
-	productDate = 0;
+	dateDay = 0;
+	dateMonth = 0;
+	dateYear = 0;
 }
